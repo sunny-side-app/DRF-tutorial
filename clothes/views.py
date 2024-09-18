51415f9eba770e8e56f43dc0473b7928aa00f382
Takahiro Ido <juve.taka@gmail.com>
2024-09-18 13:39:24 +0000

    Add models by er_diagram

--- clothes/views.py
+++ clothes/views.py
@@ -1,12 +1,27 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.response import Response
 
-from clothes.models import Clothes
-from clothes.serializers import ClothesSerializer
+# from clothes.models import Clothes
+# from clothes.serializers import ClothesSerializer
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import (
+    Clothes, Product, Order, Rating, User, Favorite, WishList, CartItem,
+    OrderItem, Payment, Shipping, Size, Target, ClothesType, Brand
+)
+from .serializers import (
+    ClothesSerializer, ProductSerializer, OrderSerializer, RatingSerializer,
+    UserSerializer, FavoriteSerializer, WishListSerializer, CartItemSerializer,
+    OrderItemSerializer, PaymentSerializer, ShippingSerializer, SizeSerializer,
+    TargetSerializer, ClothesTypeSerializer, BrandSerializer
+)
 
 
 @api_view(["GET", "POST"])
@@ -47,3 +62,227 @@ def clothes_detail(request, pk):
     elif request.method == "DELETE":
         cloth.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# # Clothes API Views
+
+# class ClothesListCreateView(generics.ListCreateAPIView):
+#     queryset = Clothes.objects.all()
+#     serializer_class = ClothesSerializer
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid():
+#             self.perform_create(serializer)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class ClothesDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Clothes.objects.all()
+#     serializer_class = ClothesSerializer
+
+#     def get_object(self):
+#         return get_object_or_404(Clothes, pk=self.kwargs.get('pk'))
+
+# Product API Views
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_object(self):
+        return get_object_or_404(Product, pk=self.kwargs.get('pk'))
+
+
+# Order API Views
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_object(self):
+        return get_object_or_404(Order, pk=self.kwargs.get('pk'))
+
+
+# Rating API Views
+
+class RatingListCreateView(generics.ListCreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class RatingDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+    def get_object(self):
+        return get_object_or_404(Rating, pk=self.kwargs.get('pk'))
+
+
+# User API Views
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return get_object_or_404(User, pk=self.kwargs.get('pk'))
+
+
+# Favorite API Views
+
+class FavoriteListCreateView(generics.ListCreateAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+
+
+class FavoriteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+
+    def get_object(self):
+        return get_object_or_404(Favorite, pk=self.kwargs.get('pk'))
+
+
+# WishList API Views
+
+class WishListListCreateView(generics.ListCreateAPIView):
+    queryset = WishList.objects.all()
+    serializer_class = WishListSerializer
+
+
+class WishListDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = WishList.objects.all()
+    serializer_class = WishListSerializer
+
+    def get_object(self):
+        return get_object_or_404(WishList, pk=self.kwargs.get('pk'))
+
+
+# CartItem API Views
+
+class CartItemListCreateView(generics.ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class CartItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+    def get_object(self):
+        return get_object_or_404(CartItem, pk=self.kwargs.get('pk'))
+
+
+# OrderItem API Views
+
+class OrderItemListCreateView(generics.ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class OrderItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+    def get_object(self):
+        return get_object_or_404(OrderItem, pk=self.kwargs.get('pk'))
+
+
+# Payment API Views
+
+class PaymentListCreateView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class PaymentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get_object(self):
+        return get_object_or_404(Payment, pk=self.kwargs.get('pk'))
+
+
+# Shipping API Views
+
+class ShippingListCreateView(generics.ListCreateAPIView):
+    queryset = Shipping.objects.all()
+    serializer_class = ShippingSerializer
+
+
+class ShippingDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shipping.objects.all()
+    serializer_class = ShippingSerializer
+
+    def get_object(self):
+        return get_object_or_404(Shipping, pk=self.kwargs.get('pk'))
+
+
+class SizeListCreateView(generics.ListCreateAPIView):
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
+
+
+class SizeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
+
+    def get_object(self):
+        return get_object_or_404(Size, pk=self.kwargs.get('pk'))
+
+# Target API Views
+class TargetListCreateView(generics.ListCreateAPIView):
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
+
+
+class TargetDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
+
+    def get_object(self):
+        return get_object_or_404(Target, pk=self.kwargs.get('pk'))
+
+
+# ClothesType API Views
+class ClothesTypeListCreateView(generics.ListCreateAPIView):
+    queryset = ClothesType.objects.all()
+    serializer_class = ClothesTypeSerializer
+
+
+class ClothesTypeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClothesType.objects.all()
+    serializer_class = ClothesTypeSerializer
+
+    def get_object(self):
+        return get_object_or_404(ClothesType, pk=self.kwargs.get('pk'))
+
+
+# Brand API Views
+class BrandListCreateView(generics.ListCreateAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class BrandDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+    def get_object(self):
+        return get_object_or_404(Brand, pk=self.kwargs.get('pk'))
